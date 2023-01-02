ba833e10939973622aace03e805e7286afcd8c29
aferullo <f3rullo14@gmail.com>
2023-01-02 15:37:41 -0800

    Included function to determine difference of burn areas over time, pre/post burn

--- nbrdetect/detect.py
+++ nbrdetect/detect.py
@@ -6,7 +6,6 @@ from numpy import ndarray
 
 from osgeo import gdal
 import rasterio as rio
-from rasterio.warp import reproject, calculate_default_transform
 
 
 def __get_band(fn:str, band:int = 1):
@@ -20,7 +19,7 @@ def __get_band(fn:str, band:int = 1):
         ndarray: A numpy array of the band extracting.
     """
     ds = gdal.Open(fn, gdal.GA_ReadOnly)
-    return ds.GetRasterBand(band).ReadAsArray().astype('float32')
+    return ds.GetRasterBand(band).ReadAsArray().astype('f4')
 
 
 def __write(fn, arr, trans, crs:str = 'EPSG:4326', nodata:int = -1):
@@ -69,15 +68,18 @@ def evaluate_band_burn_ratio(arr_nir :ndarray, arr_swir: ndarray):
     return np.nan_to_num(arr_nbr, nan=-9999)
 
 
-def evaluate_cog_burn_ratio(fn_cog:str, fn_out:str):
+def evaluate_cog_burn_ratio(fn_cog:str, fn_out:str = None):
     """Calculate the Normalized Burn Ratio of a COG and export the results to a new GeoTiff.
 
     Args:
         fn_cog (str): Cloud Optimized GeoTiff name.
-        fn_out (str): Output file ending in .tif
+        fn_out (str, optional): Output file ending in .tif. Defaults to None.
+
+    Returns:
+        ndarray, transform, crs: Data used to write a new tif
     """
-    fn_nir = '{}.B08.tif'.format(fn_cog) #'sample/HLS.S30.T11SKD.2022286T184329.v2.0.B08.tif'
-    fn_swir = '{}.B11.tif'.format(fn_cog) #'sample/HLS.S30.T11SKD.2022286T184329.v2.0.B11.tif'
+    fn_nir = '{}.B8A.tif'.format(fn_cog) #'sample/HLS.S30.T11SKD.2022286T184329.v2.0.B8A.tif'
+    fn_swir = '{}.B12.tif'.format(fn_cog) #'sample/HLS.S30.T11SKD.2022286T184329.v2.0.B12.tif'
     
     arr_nir = __get_band(fn_nir)
     arr_swir = __get_band(fn_swir)
@@ -87,7 +89,28 @@ def evaluate_cog_burn_ratio(fn_cog:str, fn_out:str):
         original_crs = str(src.crs)
         original_transform = src.transform
     
-    __write(fn_out, arr_nbr, original_transform, crs=original_crs, nodata=-9999)
+    if fn_out is not None:
+        __write(fn_out, arr_nbr, original_transform, crs=original_crs, nodata=-9999)
+        
+    return arr_nbr, original_transform, original_crs
+    
+    
+def evaluate_cog_burn_ratio_difference(fn_cog_before:str, fn_cog_after:str, fn_out:str):
+    """Calculates the difference between two COGs' NBR values.
+
+    Args:
+        fn_cog_before (str): Pre fire COG.
+        fn_cog_after (str): Post fire COG.
+        fn_out (str): Output filename.
+    """
+    arr_before, trans_before, crs_before = evaluate_cog_burn_ratio(fn_cog_before)
+    arr_after, trans_after, crs_after = evaluate_cog_burn_ratio(fn_cog_after)
+    
+    arr_mask = np.where((arr_before != -9999) & (arr_after != -9999), 1, 0)
+    arr_dif = np.subtract(arr_before, arr_after).astype('f4')
+    arr_dif_masked = np.where(arr_mask == 1, arr_dif, -9999)
+    
+    __write(fn_out, arr_dif_masked, trans_before, crs=crs_before, nodata=-9999)
 
 
 if __name__ == '__main__':
@@ -99,9 +122,12 @@ if __name__ == '__main__':
     parser.add_argument('fn_out')
     args = parser.parse_args()
     
+    # good sample to determine difference in area, pre/post fire
+    # before HLS.S30.T10SFJ.2022239T184919.v2.0
+    # after HLS.S30.T10SFJ.2022314T185621.v2.0
+    # evaluate_cog_burn_ratio_difference('sample/HLS.S30.T10SFJ.2022239T184919.v2.0', 'sample/HLS.S30.T10SFJ.2022314T185621.v2.0', 'output/burn_dif.tif')
+    
     fn_cog = args.fn_cog #'sample/HLS.S30.T11SKD.2022286T184329.v2.0'
     fn_out = args.fn_out #'output/output.tif'
-    
     evaluate_cog_burn_ratio(fn_cog, fn_out)
-
     print('Complete!')
